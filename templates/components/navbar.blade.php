3eebca8e7c6d76e2bb401f0cf47338066fe02f0b
Muhammad Ali <muhammadali.alaydrus@gmail.com>
2014-09-25 20:52:16 +0700

    [UPDATE] composer.json file

--- templates/components/navbar.blade.php
+++ templates/components/navbar.blade.php
@@ -1,106 +1,104 @@
 <nav class="nav-menu">
-    <div class="container-fluid">
-        <div class="wrapper">
-            <div class="row">
-                <div class="pull-left">
+    <div class="wrapper">
+        <div class="row">
+            <div class="pull-left">
 
-                    <!-- BRAND -->
-                    @section('brand')
-                    <h1 class="brand">
-                        <a href="{{ URL::site() }}">
-                            <span class="logo">
-                                <img src="{{ \URL::base('vendor/sleek-theme/img/xinix-logo.png') }}" alt="">
-                            </span>
-                            <span>Bono Sleek</span>
+                <!-- BRAND -->
+                @section('brand')
+                <h1 class="brand">
+                    <a href="{{ URL::site() }}">
+                        <span class="logo">
+                            <img src="{{ \URL::base('vendor/sleek-theme/img/xinix-logo.png') }}" alt="">
+                        </span>
+                        <span>Bono Sleek</span>
+                    </a>
+                </h1>
+                @show
+                <!-- END OF BRAND -->
+
+            </div>
+            <div class="pull-right">
+                <ul class="flat topbar">
+
+                    <!-- NOTIFICATION -->
+                    @section('notification')
+                    <li class="notification">
+                        <a href="#">
+                            <i class="fa fa-bell-o"></i>
+                            <span class="badge">2</span>
                         </a>
-                    </h1>
+                        <ul class="sub animated">
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-comment-o"></i>
+                                    New Comment
+                                    <span class="sub-badge pull-right">+5</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-envelope-o"></i>
+                                    New Message
+                                    <span class="sub-badge pull-right">+2</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     @show
-                    <!-- END OF BRAND -->
+                    <!-- END OF NOTIFICATION -->
 
-                </div>
-                <div class="pull-right">
-                    <ul class="flat topbar">
+                    <!-- USER NAVIGATION -->
+                    @section('user.navigation')
+                    <li class="user">
 
-                        <!-- NOTIFICATION -->
-                        @section('notification')
-                        <li class="notification">
-                            <a href="#">
-                                <i class="fa fa-bell-o"></i>
-                                <span class="badge">2</span>
-                            </a>
-                            <ul class="sub animated">
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-comment-o"></i>
-                                        New Comment
-                                        <span class="sub-badge pull-right">+5</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-envelope-o"></i>
-                                        New Message
-                                        <span class="sub-badge pull-right">+2</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
+                        <!-- USER NAVIGATION AVATAR / IMAGE -->
+                        @section('user.navigation.avatar')
+                        <a href="#">
+                            <div class="avatar" style="background: url('{{ \URL::base('vendor/sleek-theme/img/xinix-logo.png') }}') center no-repeat; background-size: cover;"></div>
+                            <span>
+                                <small>Welcome, </small>
+                                Admin
+                            </span>
+                            <i class="fa fa-caret-down"></i>
+                        </a>
                         @show
-                        <!-- END OF NOTIFICATION -->
+                        <!-- END OF USER NAVIGATION AVATAR / IMAGE -->
 
-                        <!-- USER NAVIGATION -->
-                        @section('user.navigation')
-                        <li class="user">
+                        <!-- USER NAVIGATION MENU / DROPDOWN -->
+                        @section('user.navigation.menu')
+                        <ul class="sub animated">
 
-                            <!-- USER NAVIGATION AVATAR / IMAGE -->
-                            @section('user.navigation.avatar')
-                            <a href="#">
-                                <div class="avatar" style="background: url('{{ \URL::base('vendor/sleek-theme/img/xinix-logo.png') }}') center no-repeat; background-size: cover;"></div>
-                                <span>
-                                    <small>Welcome, </small>
-                                    Admin
-                                </span>
-                                <i class="fa fa-caret-down"></i>
-                            </a>
+                            <!-- USER NAVIGATION CHILD MENU -->
+                            @section('user.navigation.menu.children')
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-user"></i>
+                                    Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-cogs"></i>
+                                    Settings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-sign-out"></i>
+                                    Logout
+                                </a>
+                            </li>
                             @show
-                            <!-- END OF USER NAVIGATION AVATAR / IMAGE -->
+                            <!-- END OF USER NAVIGATION CHILD MENU -->
 
-                            <!-- USER NAVIGATION MENU / DROPDOWN -->
-                            @section('user.navigation.menu')
-                            <ul class="sub animated">
-
-                                <!-- USER NAVIGATION CHILD MENU -->
-                                @section('user.navigation.menu.children')
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-user"></i>
-                                        Profile
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-cogs"></i>
-                                        Settings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-sign-out"></i>
-                                        Logout
-                                    </a>
-                                </li>
-                                @show
-                                <!-- END OF USER NAVIGATION CHILD MENU -->
-
-                            </ul>
-                            @show
-                            <!-- END OF USER NAVIGATION MENU / DROPDOWN -->
-                        </li>
+                        </ul>
                         @show
-                        <!-- END OF USER NAVIGATION -->
+                        <!-- END OF USER NAVIGATION MENU / DROPDOWN -->
+                    </li>
+                    @show
+                    <!-- END OF USER NAVIGATION -->
 
-                    </ul>
-                </div>
+                </ul>
             </div>
         </div>
     </div>
--- templates/components/sidebar.blade.php
+++ templates/components/sidebar.blade.php
@@ -3,16 +3,16 @@
         @foreach(App::getInstance()->config('navbar.menus') ?: array() as $uri)
             @if(! isset($uri['children'])) <!-- NO CHILD -->
                 <li>
-                    <a href="{{ URL::site($uri['uri']) }}">{{ @$uri['icon'] }} &nbsp;&nbsp; {{ @$uri['title'] }}</a>
+                    <a href="{{ URL::site($uri['uri']) }}">{{ @$uri['icon'] }} &nbsp;&nbsp;{{ @$uri['title'] }}</a>
                 </li>
             @else <!-- HAS CHILD -->
                 <li class="collapsible">
-                    <a href="#"><span class="pull-left">{{ @$uri['icon'] }}</span> &nbsp;&nbsp; {{ @$uri['title'] }} <i class="icon">+</i></a>
+                    <a href="#"><span class="pull-left">{{ @$uri['icon'] }}</span> &nbsp;&nbsp;{{ @$uri['title'] }} <i class="icon">+</i></a>
                     <ul class="hide animated fadeOutUp">
                         @foreach (@$uri['children'] as $uri)
                             <li>
                                 <a href="{{ URL::site($uri['uri']) }}">
-                                    {{ @$uri['icon'] }} &nbsp;&nbsp; {{ @$uri['title'] }}
+                                    {{ @$uri['icon'] }}&nbsp;&nbsp;{{ @$uri['title'] }}
                                 </a>
                             </li>
                         @endforeach
