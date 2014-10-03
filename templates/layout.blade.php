9147ea1d0eb2eb61ee8976414c1fde48e635dc0c
Krisan Alfa Timur <krisan47@gmail.com>
2014-10-03 14:55:55 +0700

    [UPDATE] Add option to choose base color.

--- templates/layout.blade.php
+++ templates/layout.blade.php
@@ -25,7 +25,7 @@
     <!-- END OF PAGE LEVEL STYLING -->
 </head>
 
-<body>
+<body class="@yield('body.class', '')">
     <!-- NAVBAR -->
     @section('navbar')
         @include('components.navbar')
